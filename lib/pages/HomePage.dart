93adb0d6c55bcf87e50185f915df7681f7cbf590
Murewa Ajala <murewatajala@gmail.com>
2024-05-23 11:00:56 +0100

    Page 4

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -2,36 +2,128 @@ import 'package:flutter/material.dart';
 import 'LandingPage.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    Color myColor = Color.fromRGBO(210, 217, 209, 1);
-    Color myColor2 = Color.fromRGBO(82, 130, 101, 1);
-    Color myColor3 = Color.fromRGBO(0, 0, 0, 0.65);
-    Color myColor4 = Color.fromRGBO(0, 0, 0, 1);
+  _HomepageState createState() => _HomepageState();
+}
 
+class _HomepageState extends State<Homepage> {
+  Color myColor = Color.fromRGBO(210, 217, 209, 1);
+  Color myColor2 = Color.fromRGBO(82, 130, 101, 1);
+  Color myColor3 = Color.fromRGBO(0, 0, 0, 0.65);
+  Color myColor4 = Color.fromRGBO(0, 0, 0, 1);
+
+  
+  List<bool> _isSelected = [false, false, false];
+ 
+  void _onButtonTap(int index) {
+    setState(() {
+      for (int i = 0; i < _isSelected.length; i++) {
+        _isSelected[i] = i == index;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           children: [
             Padding(
-              padding: EdgeInsets.only(left: 62.0, right: 62.0, top: 60.0, bottom: 30.0),
-              child: Row(
-                children: [
-                  Image.asset(
-                    'Assets/Images/chemba.png',
-                    height: 100,
-                  ),
-                ],
+              padding: EdgeInsets.only(left: 20.0, top: 20.0),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Image.asset(
+                  'Assets/Images/chemba.png',
+                  height: 118,
+                ),
               ),
             ),
+            SizedBox(height: 40),  
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () => _onButtonTap(0),
+                  child: Container(
+                    width: 100,  
+                    height: 60,  
+                    margin: EdgeInsets.symmetric(horizontal: 10),  
+                    decoration: BoxDecoration(
+                      color: _isSelected[0] ? myColor2 : Colors.transparent,
+                      border: _isSelected[0] ? null : Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Events',
+                      style: TextStyle(
+                        fontFamily: 'Manrope',
+                        fontWeight: FontWeight.w700,
+                        color: _isSelected[0] ? Colors.white : Colors.black,
+                        fontSize: 19,
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => _onButtonTap(1),
+                  child: Container(
+                   width: 120,  
+                    height: 60, 
+                    margin: EdgeInsets.symmetric(horizontal: 10),  
+                    decoration: BoxDecoration(
+                      color: _isSelected[1] ? myColor2 : Colors.transparent,
+                      border: _isSelected[1] ? null : Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'FAQ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontFamily: 'Manrope',
+                        color: _isSelected[1] ? Colors.white : Colors.black,
+                        fontSize: 19,
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => _onButtonTap(2),
+                  child: Container(
+                    width: 120,  
+                    height: 60, 
+                    margin: EdgeInsets.symmetric(horizontal: 10), 
+                    decoration: BoxDecoration(
+                      color: _isSelected[2] ? myColor2 : Colors.transparent,
+                      border: _isSelected[2] ? null : Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Educate',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontFamily: 'Manrope',
+                        color: _isSelected[2] ? Colors.white : Colors.black,
+                        fontSize: 19,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),  
+
           ],
         ),
       ),
-   bottomNavigationBar: ClipRRect(
+      bottomNavigationBar: ClipRRect(
         borderRadius: BorderRadius.only(
           topLeft: Radius.circular(20.0),
           topRight: Radius.circular(20.0),
@@ -79,7 +171,6 @@ class Homepage extends StatelessWidget {
           ),
         ),
       ),
-    
     );
   }
 }
